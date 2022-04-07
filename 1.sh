ee81ea4192bbfff0b45a037f0ca4975b4b026048
krozis <stelie@student.42.fr>
2022-04-07 13:25:21 +0200

    added 1.sh, no sudo_pwd dir (yet)

--- 1.sh
+++ 1.sh
@@ -23,17 +23,17 @@ systemctl enable ufw
 systemctl restart ufw
 
 #Configure sudo
-mkdir /var/log/sudo
-cp sudo_pwd/sudoconfig /etc/sudoers.d/
+#mkdir /var/log/sudo
+#cp sudo_pwd/sudoconfig /etc/sudoers.d/
 
 #Password Policy
-$S="PASS_MAX_DAYS	99999"
-$R="PASS_MAX_DAYS	30"
-$FILE="/etc/login.defs"
-sed -i "s/$S/$R/" $FILE
-$S="PASS_MIN_DAYS	0"
-$R="PASS_MIN_DAYS	3"
-sed -i "s/$S/$R/" $FILE
-apt install libpam-pwquality -y
-cp sudo_pwd/common-password /etc/pam.d/
+#$S="PASS_MAX_DAYS	99999"
+#$R="PASS_MAX_DAYS	30"
+#$FILE="/etc/login.defs"
+#sed -i "s/$S/$R/" $FILE
+#$S="PASS_MIN_DAYS	0"
+#$R="PASS_MIN_DAYS	3"
+#sed -i "s/$S/$R/" $FILE
+#apt install libpam-pwquality -y
+#cp sudo_pwd/common-password /etc/pam.d/
 
