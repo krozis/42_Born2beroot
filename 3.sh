057e0d14b1c6a7bca603845f097fc2163affe9f7
krozis <stelie@student.42.fr>
2022-04-07 14:55:53 +0200

    corrected 3.sh missing -y in apt

--- 3.sh
+++ 3.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-apt install lighttpd
-apt install mariadb-server
+apt install lighttpd -y
+apt install mariadb-server -y
 echo ""
 echo "Next is Mariadb config: n til 'Remove anonymous users?' then Y"
 mysql_secure_installation
