9ac97b083e089dc77f46babbfb8a232b5fa769b8
Stephen ELIE <stelie@student.42.fr>
2022-04-11 11:31:36 +0200

    corrected 3.sh -> sed in wp-config.php

--- 3.sh
+++ 3.sh
@@ -19,9 +19,9 @@ rm /var/www/html/latest.tar.gz
 cp -r /var/www/html/wordpress/* /var/www/html
 rm -rf /var/www/html/wordpress
 cp /var/www/html/wp-config-sample.php $FILE
-sed -i "s/'database_name_here'/$DBNAME/" $FILE
-sed -i "s/'username_here'/$UNAME/" $FILE
-sed -i "s/'password_here'/$PWD/" $FILE
+sed -i "s/database_name_here/$DBNAME/" $FILE
+sed -i "s/username_here/$UNAME/" $FILE
+sed -i "s/password_here/$PWD/" $FILE
 sudo chown -R www-data:www-data /var/www/html/
 sudo chmod -R 755 /var/www/html/
 sudo lighty-enable-mod fastcgi
